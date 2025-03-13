38b422828f7a57a04b5bf64f092a3aa1490c8b37
Paul Kirkaas <pkirkaas@gmail.com>
2025-03-13 04:08:27 +0100

    refactor: Simplify layout and remove footer in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,12 @@ import {openrouterModels} from "@/data/openrouter-models";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="outline outline-solid outline-1">
+      <main className="border border-black outline outline-solid outline-1">
         <h1>The Main</h1>
         <DynamicTable data={openrouterModels} />
 
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <div> The Footer</div>
-      </footer>
     </div>
   );
 }
