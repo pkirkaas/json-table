532dd4d53b0bf86e324000ec05e8ee269f3e375b
Paul Kirkaas <pkirkaas@gmail.com>
2025-03-15 01:23:08 +0100

    feat: Add initial column visibility state to DynamicTable component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
-import {DynamicTable} from "@/app/components/DynamicTable";
-import {openrouterModels} from "@/data/openrouter-models";
+import { DynamicTable } from "@/app/components/DynamicTable";
+import { openrouterModels } from "@/data/openrouter-models";
+
+let initialState = {
+  columnVisibility: {
+    architecture: false,
+    per_request_limits: false,
+    top_provider: false,
+    createdAt: false,
+    random:false,
+  },
+};
 
 export default function Home() {
   return (
     <div className="mx-auto p-4 h-screen flex flex-col">
       <main className="flex-1 flex flex-col min-h-0">
         <div className="flex-1 h-full">
-          <DynamicTable data={openrouterModels} title="Open Router Models"/>
+          <DynamicTable data={openrouterModels} title="Open Router Models" initialState={initialState}/>
         </div>
       </main>
     </div>
